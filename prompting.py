5754b2198cd02add0e76b1803d8901d8c4442efd
Rachana-choudhary <146935416+Rachana-choudhary@users.noreply.github.com>
2023-11-01 20:37:46 +0530

    Update prompting.py

--- prompting.py
+++ prompting.py
@@ -13,7 +13,7 @@ def generate_controls(control_description):
     text = f""""Review the description of the operational risk control delimited by triple backticks and provide a detailed feedback on the content in alignment with best practices to document operational risk controls. Your feedback should include the positive aspects of the provided control description, feedback on spelling and grammar mistakes, feedback on gaps in the description and guidance to address these, and revised version of the description which aligns with best practices. {control_description} Please provide the output in where all the headings should be in BOLD and paragraph are in normal font and please make sure to not include anything about it in the output"""
 
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4",
         messages=[{"role": "user", "content": text}],
         temperature=0.6,
         max_tokens=3000,
